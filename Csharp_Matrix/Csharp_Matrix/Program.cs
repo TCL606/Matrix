74227f02747097032a7a8ae09c951a440c7a82a3
TCL <1620508360@qq.com>
2022-01-02 00:21:39 +0800

    chore: add Csharp matrix

---
+++ Csharp_Matrix/Csharp_Matrix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TCL_Matrix;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Matrix m = new Matrix(new double[,]{
+                { 1,1,1,1},
+                { 1,2,3,4},
+                { 1,3,6,10},
+                { 1,3,6,10},
+            });
+            Matrix? bases;
+            m.GetBasesOfNullSpace(out bases);
+            bases?.Display();
+        }
+    }
+}
