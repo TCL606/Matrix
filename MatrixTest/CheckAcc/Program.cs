9076de366f294b2c6bda4af69c9cfdecd44aa00d
TCL <1620508360@qq.com>
2022-02-11 22:34:48 +0800

    chore: add acc checker

---
+++ MatrixTest/CheckAcc/Program.cs
@@ -0,0 +1,15 @@
+﻿using MatrixTest;
+
+namespace Result
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine($"LUAcc:{UnitTest.LUAcc()}");
+            //Console.WriteLine($"QRAcc:{UnitTest.QRAcc()}");
+            Console.WriteLine($"SVDAcc:{UnitTest.SVDAcc()}");
+            //Console.WriteLine($"EigenAcc:{UnitTest.EigenAcc()}");
+        }
+    }
+}
--- MatrixTest/MatrixTest/Test.cs
+++ MatrixTest/MatrixTest/Test.cs
@@ -3,27 +3,34 @@ using System;
 
 namespace MatrixTest
 {
-    public partial class UnitTest
+    public static partial class UnitTest
     {
         [TestMethod]
-        public void LUTest()
+        public static void LUTest()
         {
             double acc = LUAcc();
             Assert.IsTrue(acc == 1.0);
         }
 
         [TestMethod]
-        public void QRTest()
+        public static void QRTest()
         {
             double acc = QRAcc();
             Assert.IsTrue(acc == 1.0);
         }
 
         [TestMethod]
-        public void SVDTest()
+        public static void SVDTest()
         {
             double acc = SVDAcc();
             Assert.IsTrue(acc > 0.55); //SVD的准确率其实不到六成
+        }
+
+        [TestMethod]
+        public static void EigenTest()
+        {
+            double acc = EigenAcc();
+            Assert.IsTrue(acc > 0.6);
         }
     }
 }
