1e65112c6148e699908dc29c966d440f76a568f9
TCL <1620508360@qq.com>
2022-02-11 20:01:16 +0800

    chore: add test

---
+++ MatrixTest/MatrixTest/Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MatrixTest
+{
+    public partial class UnitTest
+    {
+        [TestMethod]
+        public void LUTest()
+        {
+            double acc = LUAcc();
+            Assert.IsTrue(acc == 1.0);
+        }
+
+        [TestMethod]
+        public void QRTest()
+        {
+            double acc = QRAcc();
+            Assert.IsTrue(acc == 1.0);
+        }
+
+        [TestMethod]
+        public void SVDTest()
+        {
+            double acc = SVDAcc();
+            Assert.IsTrue(acc > 0.55); //SVD的准确率其实不到六成
+        }
+    }
+}
